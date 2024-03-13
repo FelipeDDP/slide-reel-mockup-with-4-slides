0dc19566bcd6a69238a9b5b6fb4dff664d2abf66
Felipe de Paula <fdiasdepaula@gmail.com>
2024-03-13 11:39:06 +0100

    new improved javascript with toggle and math.max/min methods

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,20 @@
-const images = document.querySelectorAll('.slide')
-const arrowBack = document.getElementById('back-arrow')
-const arrowNext = document.getElementById('next-arrow')
+const images = document.querySelectorAll('.slide');
+const arrowBack = document.getElementById('back-arrow');
+const arrowNext = document.getElementById('next-arrow');
+let i = 0;
 
-let i = 0
+const toggleArrowsOpacity = () => {
+    arrowBack.classList.toggle('opacity', i === 0);
+    arrowNext.classList.toggle('opacity', i === images.length - 1);
+};
 
-const advanceSlide = function() {
-    if (i < images.length - 1) {
-        images[i].classList.remove('show');
-        images[i].classList.add('hidden')
-        i++;
-        images[i].classList.remove('hidden')
-        images[i].classList.add('show')
-    }
+const changeSlide = (direction) => {
+    images[i].classList.remove('show');
+    i += direction;
+    i = Math.max(0, Math.min(i, images.length - 1));
+    images[i].classList.add('show');
+    toggleArrowsOpacity();
+};
 
-    if (i != 0) {
-        arrowBack.classList.remove('opacity')
-    }
-
-    if (i == images.length - 1) {
-        arrowNext.classList.add('opacity')
-    }
-}
-
-const returnSLide = function() {
-    if (i != 0) {
-        images[i].classList.remove('show');
-        images[i].classList.add('hidden')
-        i--;
-        images[i].classList.add('show')
-        images[i].classList.remove('hidden')
-    }
-
-    if (i < images.length - 1) {
-        arrowNext.classList.remove('opacity')
-    }
-
-    if (i === 0) {
-        arrowBack.classList.add('opacity')
-    }
-}
-
-arrowNext.addEventListener('click', advanceSlide)
-
-arrowBack.addEventListener('click', returnSLide)
+arrowNext.addEventListener('click', () => changeSlide(1));
+arrowBack.addEventListener('click', () => changeSlide(-1));
